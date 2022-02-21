7c344a17063917fa1b634046412d40b2061eb044
Stephie <stephaniecrocker91@gmail.com>
2022-02-21 11:37:18 +0000

    Update data for laptops

---
+++ assets/py/script.py
@@ -0,0 +1,4 @@
+imacDictionary = {
+    "GL_iMac_4": {"serial": "C02H775WDHJP", "location": "london", "desk allocations": "0", "user": "Stephanie Crocker", "leased": "no"}
+    "GL_iMac_42": {"serial": "C02TG0F8J1GG", "location": "london", "desk allocations": "2", "user": "Darek Czerwonka", "leased": "no"}
+}
