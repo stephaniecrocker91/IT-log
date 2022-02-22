58b5ee2d70dc70b11bfcd3e4e560996fa023cf59
Stephie <stephaniecrocker91@gmail.com>
2022-02-22 14:54:04 +0000

    Add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,48 @@
+/* Global variables*/
+let allImacs = document.getElementById("all-imacs");
+let portoImacs = document.getElementsByClassName("porto-imacs");
+let londonImacs = document.getElementsByClassName("london-imacs");
+let leasedImacs = document.getElementsByClassName("leased-imacs");
+let homeOfficeImacs = document.getElementsByClassName("homeoffice-imacs");
 /* get London imac Button*/
-
+let allImacButton = document.getElementById("imac-button-all");
 let londonImacButton = document.getElementById("imac-button-london");
+let portoImacButton = document.getElementById("imac-button-porto");
 
-londonImacButton.addEventListener('click', displayNone);
 
-function displayNone(e) {
-    let portoImacs = document.getElementsByClassName("porto")
+
+/* Event listeners*/
+allImacButton.addEventListener('click',displayAllImacs)
+londonImacButton.addEventListener('click', displayLondonImacs);
+portoImacButton.addEventListener('click', displayPortoImacs);
+
+
+/** 
+ * Function to display all iMacs
+ * */
+ function displayAllImacs(e) {
+
+}
+
+
+
+/** 
+ * Function to display only London iMacs
+ * */
+function displayLondonImacs(e) {
     for (let i of portoImacs) {
+        i.style.display="none";
+    }  
+}
+/** 
+ * Function to display only Porto iMacs
+ * */
+ function displayPortoImacs(e) {
+ 
+    for (let i of londonImacs) {
         i.style.display="none";
     }
 }
+
+
     
