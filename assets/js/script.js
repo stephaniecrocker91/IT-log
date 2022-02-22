8ae51de8b193b1d5ae6ecb5c0711ce08f347390f
Stephie <stephaniecrocker91@gmail.com>
2022-02-22 13:39:22 +0000

    Add code to js file, event listener for London button which hides display of Porto imac when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-/* get button to show Porto iMacs Only*/
+/* get London imac Button*/
+
+let londonImacButton = document.getElementById("imac-button-london");
+
+londonImacButton.addEventListener('click', displayNone);
+
+function displayNone(e) {
+    let portoImacs = document.getElementsByClassName("porto")
+    for (let i of portoImacs) {
+        i.style.display="none";
+    }
+}
+    
